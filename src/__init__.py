053f0b23708fa721764029f05efc1344f3c8afb0
phantom2059 <sergeykattsyn2008@gmail.com>
2025-05-26 19:25:10 +0700

    Initial commit: Базовая структура проекта классификации животных

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+"""
+Пакет для классификации животных по фотографиям
+"""
+
+from .animal_classifier import AnimalClassifier
+
+__version__ = "1.0.0"
+__author__ = "Ваше имя"
+
+__all__ = ["AnimalClassifier"] 
